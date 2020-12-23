d6180242431a83f7bdfbbd3c4a9444e9bcdb3217
Andreas Veithen <andreas.veithen@gmail.com>
2020-12-23 14:03:25 +0000

    Update parent POM

--- src/test/java/com/github/veithen/hermetic/HermeticSecurityManagerTest.java
+++ src/test/java/com/github/veithen/hermetic/HermeticSecurityManagerTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,7 @@ public class HermeticSecurityManagerTest {
     public static void installSecurityManager() {
         HermeticSecurityManager.install();
     }
-    
+
     @AfterClass
     public static void uninstallSecurityManager() {
         HermeticSecurityManager.uninstall();
@@ -38,7 +38,7 @@ public class HermeticSecurityManagerTest {
     // FilePermission and SocketPermission entries, and other permissions are granted
     // by the custom SecurityManager. Using a different one would then inevitable
     // result in security exceptions.
-    @Test(expected=SecurityException.class)
+    @Test(expected = SecurityException.class)
     public void testSetSecurityManager() {
         System.setSecurityManager(new SecurityManager());
     }
